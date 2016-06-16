072769eca5304cbfc3c59e4bff196fa44f2fcf54
Morten Noddeland <morten@noddeland.no>
2016-06-16 12:54:55 +0200

    Add two frameworks (Foundation and Belle) and check that they work

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button, ComboBox, Option, DatePicker, TextInput } from 'belle';
 
-import '../public/styles/app.scss';
+import './styles/main.scss';
+
+class ExampleTextInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            exampleText: 'Hei'
+        };
+
+        this.handleTextChange = this.handleTextChange.bind(this);
+        this.inputValueLink = this.inputValueLink.bind(this);
+    }
+
+    inputValueLink() {
+        return {
+            value: this.state.exampleText,
+            requestChange: this.handleTextChange
+        };
+    }
+
+    handleTextChange(newValue) {
+        this.setState({ exampleText: newValue });
+    }
+
+    render() {
+        return (
+            <div>
+                <TextInput
+                    valueLink={this.inputValueLink()}
+                    placeholder="Just fill in whatever you like :)"
+                />
+
+                <p><strong>Two-way data binding:</strong> {this.state.exampleText}</p>
+            </div>
+        );
+    }
+}
+
+function FoundationTest() {
+    return (
+        <div className="callout success">
+            <h5>This is a success callout</h5>
+            <p>It has an easy to override visual style, and is appropriately subdued.</p>
+            <a href="#">It's dangerous to go alone, take this.</a>
+        </div>
+    );
+}
 
 function HelloWorld() {
     return (
-        <h1>Hei, Capra!</h1>
+        <div>
+            <Button>Hei</Button>
+            <ComboBox placeholder="Choose a State">
+                <Option value="Alabama">Alabama</Option>
+                <Option value="Alaska">Alaska</Option>
+                <Option value="Arizona">Arizona</Option>
+                <Option value="Arkansas">Arkansas</Option>
+            </ComboBox>
+            <DatePicker />
+            <ExampleTextInput />
+            <FoundationTest />
+        </div>
     );
 }
 
