b5c44f47f4c19aa378fed39bc40da18262d64c6d
Kristoffer Larsen <kristoffer@larsen.so>
2016-06-17 12:46:07 +0200

    updated circle

--- test/steps/then.js
+++ test/steps/then.js
@@ -5,16 +5,43 @@ import chaiAsPromised from 'chai-as-promised';
 chai.Should();
 chai.use(chaiAsPromised);
 
+const client = require('webdriverio').remote({
+    desiredCapabilities: {
+        browserName: 'firefox'
+    }
+});
+
+
 module.exports = function () { // eslint-disable-line
-    this.Then(/^I expect that element "(.*)" does exist$/, (selector) =>
-        browser.elements(selector).value.should.have.length.above(0)
+    this.Then(/^I expect that element "(.*)" does exist$/, (selector, done) =>
+        client
+            .waitForExist(selector, 1000)
+            .elements(selector)
+            .then(elements => {
+                console.log('aofjaopsfkoajgoakfomaofgmakgmafwmqgkomafmopmgkosamkgamkomagod');
+                console.log(elements);
+                done();
+            })
     );
 
-    this.Then(/^I expect that element "(.*)" does not exist$/, (selector) =>
-        browser.elements(selector).value.should.deep.equal([])
+    this.Then(/^I expect that element "(.*)" does not exist$/, (selector, done) =>
+        client
+            .waitForExist(selector, 1000, true)
+            .elements(selector)
+            .then(elements => {
+                console.log('aofjaopsfkoajgoakfomaofgmakgmafwmqgkomafmopmgkosamkgamkomagod');
+                console.log(elements);
+                done();
+            })
     );
 
-    this.Then(/^I expect that title is equal to "(.*)"$/, (title) =>
-        browser.getTitle().should.be.equal(title)
+    this.Then(/^I expect that title is equal to "(.*)"$/, (title, done) =>
+        client
+            .getTitle()
+            .then(response => {
+                console.log(response);
+                console.log(title);
+                done();
+            })
     );
 };
