a2abbcfa613a1d60d69dc2f494d61e8917625d70
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-06-19 17:19:41 +0200

    Add form data handling
    See DIH-90

--- src/sections/destinations/NewDestinationForm.jsx
+++ src/sections/destinations/NewDestinationForm.jsx
@@ -4,10 +4,15 @@ class NewDestinationForm extends React.Component {
     render() {
         return (<form id="newDestinationForm">
             <label for="destinationName">Name of destination:</label>
-            <input type="text" id="destinationName" />
-            <button type="submit">Add</button>
+            <input type="text" id="destinationName" ref={input => { this._name = input; } } />
+            <button onClick="{this._handleSubmit.bind(this)}" type="submit">Add</button>
             </form>
         );
+    }
+
+    _handleSubmit(event) {
+        event.preventDefault();
+        // @TODO Push data to remote server
     }
 }
 
