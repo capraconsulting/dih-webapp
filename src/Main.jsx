bd660a9f4a81914952fc5189025fb548dde5809a
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-06-19 23:04:18 +0200

    Add react-router

---
+++ src/Main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './styles/main.scss';
+
+import Header from './commons/Header.jsx';
+import Sidebar from './commons/Sidebar.jsx';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="wrapper">
+                    <Sidebar />
+                    <div className="main-content">
+                        {this.props.children}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
