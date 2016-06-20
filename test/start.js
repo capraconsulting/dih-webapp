e4136b82e567a06698f634b32728d5e92d294e24
Kristoffer Larsen <kristoffer@larsen.so>
2016-06-20 08:10:07 +0200

    update circle

--- test/start.js
+++ test/start.js
@@ -2,14 +2,3 @@ const child = require('child_process');
 child.spawn('npm', ['run', 'selenium', 'start'], {
     detached: true
 });
-child.spawn('npm', ['run', 'dev'], {
-    detached: true
-});
-
-setTimeout(() => {
-    child.exec('wdio test/wdio.conf.js', (err, stdout, stderr) => {
-        console.log(err); // eslint-disable-line
-        console.log(stdout); // eslint-disable-line
-        console.log(stderr); // eslint-disable-line
-    });
-}, 5000);
