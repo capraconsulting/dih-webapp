4cd2222a9a631d158f05bdcba203cdc7d5bcf872
Morten Noddeland <morten@noddeland.no>
2016-06-29 12:41:53 +0200

    Demo of notification using 'react-notification-system'

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
 import '../styles/main.scss';
 
+import NotificationSystem from 'react-notification-system';
+
 import Header from '../commons/Header.jsx';
 import Sidebar from '../commons/Sidebar.jsx';
 
-function MainLayout(props) {
-    return (
-        <div>
-            <Header />
-            <div className="wrapper">
-                <Sidebar />
-                <div className="main-content">
-                    {props.children}
+class MainLayout extends React.Component {
+    constructor(props) {
+        super(props);
+        this.notificationSystem = null;
+    }
+
+    componentDidMount() {
+
+        this.notificationSystem = this.refs.notificationSystem;
+        console.log(this.notificationSystem);
+    }
+
+    addNotification(event) {
+        event.preventDefault();
+        this.notificationSystem.addNotification({
+            message: 'Notification message',
+            level: 'success'
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="wrapper">
+                    <Sidebar />
+                    <div className="main-content">
+                        <button onClick={this.addNotification.bind(this)}>Add notification</button>
+                        {this.props.children}
+                    </div>
                 </div>
+                <NotificationSystem ref="notificationSystem" />
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 MainLayout.propTypes = {
