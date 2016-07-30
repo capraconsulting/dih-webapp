fa0d856c80a3502ee00c008646afeffad220ecb2
Stein-Otto Svorstøl <steinotto@svorstol.com>
2016-07-30 14:56:20 +0200

    Add new fields to edit trip form
    See DIH-230

--- src/sections/trips/trip/tripForm.jsx
+++ src/sections/trips/trip/tripForm.jsx
@@ -2,17 +2,81 @@ import React from 'react';
 import DatePicker from 'react-datepicker';
 import { reduxForm } from 'redux-form';
 import moment from 'moment';
+import { TRAVEL_METHODS } from '../../../constants';
+import SelectField from '../../../commons/Form/SelectField';
 
-const fields = ['startDate', 'endDate', 'hotel', 'notes'];
+
+const fields = [
+    'startDate',
+    'endDate',
+    'hotel',
+    'notes',
+    'travelMethod',
+    'otherTravelInformation',
+    'departureAirport',
+    'flightNumber',
+    'arrivalDate',
+    'departureDate'
+];
 
 function TripForm(props) {
     const {
-        fields: { startDate, endDate, hotel, notes },
+        fields: {
+            startDate,
+            endDate,
+            hotel,
+            notes,
+            travelMethod,
+            otherTravelInformation,
+            departureAirport,
+            flightNumber,
+            arrivalDate,
+            departureDate
+        },
         trip,
         handleSubmit,
         submitting
     } = props;
+    let travelInformationFields = ''; // Fields for volunteers travel info
 
+    const updateTravelInformationFields = () => {
+        if (travelMethod.value === TRAVEL_METHODS.PLANE) {
+            travelInformationFields = (<div>
+                <div className="field">
+                    <label htmlFor="departureAirport">Aiport of flight departure</label>
+                    <input
+                        type="text"
+                        id="departureAirport"
+                        {...departureAirport}
+                    />
+                </div>
+                <div className="field">
+                    <label htmlFor="flightNumber">Flight number</label>
+                    <input
+                        type="text"
+                        id="flightNumber"
+                        {...flightNumber}
+                    />
+                </div>
+            </div>);
+        } else if (travelMethod.value === TRAVEL_METHODS.OTHER) {
+            travelInformationFields = (
+                <div className="field">
+                    <label htmlFor="otherTravelInformation">
+                        Other travel information
+                    </label>
+                    <textarea
+                        id="otherTravelInformation"
+                        placeholder="Describe how you're going to travel to the destination"
+                        {...otherTravelInformation}
+                    />
+                </div>
+            );
+        } else { travelInformationFields = ''; }
+        return travelInformationFields;
+    };
+
+    updateTravelInformationFields(); // Set area on intial load
     return (
         <form id="tripForm" className="ui form" onSubmit={handleSubmit}>
             <div className="field">
@@ -44,7 +108,56 @@ function TripForm(props) {
             </div>
 
             <div className="field">
-                <label htmlFor="hotel">Hotel</label>
+                <label htmlFor="departureDate">Date of departure towards the destination</label>
+                <DatePicker
+                    {...departureDate}
+                    dateFormat="YYYY-MM-DD"
+                    placeholderText="YYYY-MM-DD"
+                    minDate={departureDate.value ? moment(departureDate.value) : moment()}
+                    selected={departureDate.value ?
+                        moment(departureDate.value) : moment(trip.departureDate)}
+                    allowNullValue
+                    id="departureDate"
+                    locale="en-gb"
+                />
+            </div>
+
+            <div className="field">
+                <label htmlFor="arrivalDate">Date of arrival at destination</label>
+                <DatePicker
+                    {...arrivalDate}
+                    dateFormat="YYYY-MM-DD"
+                    placeholderText="YYYY-MM-DD"
+                    minDate={moment()}
+                    selected={arrivalDate.value ?
+                        moment(arrivalDate.value) : moment(trip.arrivalDate)}
+                    allowNullValue
+                    id="arrivalDate"
+                    locale="en-gb"
+                />
+            </div>
+
+            <SelectField
+                label="Method of travel"
+                values={[
+                    { name: TRAVEL_METHODS.PLANE },
+                    { name: TRAVEL_METHODS.OTHER }
+                ]}
+                placeholder="Select method of travel"
+                allowNullValue
+                valueLabel="name"
+                valueKey="name"
+                onInput={() => {
+                    updateTravelInformationFields();
+                }}
+            >
+            {travelMethod}
+            </SelectField>
+
+            {travelInformationFields}
+
+            <div className="field">
+                <label htmlFor="hotel">Hotel you're going to stay at</label>
                 <input
                     type="text"
                     id="hotel"
@@ -57,6 +170,7 @@ function TripForm(props) {
                 <textarea
                     {...notes}
                     value={notes.value || ''}
+                    id="notes"
                 />
             </div>
 
