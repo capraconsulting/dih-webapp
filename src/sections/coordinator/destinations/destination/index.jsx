368ab1e9b5f3fa4578bbe1059b0cfc7d09cb876f
Morten Noddeland <morten@noddeland.no>
2016-08-04 16:00:17 +0200

    Forgot to add these files in previous commit

---
+++ src/sections/coordinator/destinations/destination/index.jsx
@@ -0,0 +1,57 @@
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+import { retrieve } from '../../../../actions/destinationActions';
+import Header from '../../../../commons/pageHeader';
+import Navbar from '../../../../commons/navbar';
+
+const createHandlers = (dispatch) => (id) => dispatch(retrieve(id));
+
+class Destination extends Component {
+    constructor(props) {
+        super(props);
+        this.handlers = createHandlers(this.props.dispatch);
+        this.state = {
+            pages: [
+                {
+                    name: 'Volunteers',
+                    uri: `/coordinator/destinations/${this.props.params.destinationId}`
+                },
+                {
+                    name: 'Add volunteer',
+                    uri: `/coordinator/destinations/${this.props.params.destinationId}/addvolunteer`
+                }
+            ]
+        };
+    }
+
+    componentDidMount() {
+        this.handlers(this.props.params.destinationId);
+    }
+
+    render() {
+        return (
+            <div className="ui segment clearing">
+                <Header
+                    icon="marker"
+                    content={this.props.destination.name}
+                    subContent="Manage destination"
+                />
+                <Navbar pages={this.state.pages} />
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = store => ({
+    destination: store.destinationState.destination
+});
+
+Destination.propTypes = {
+    children: PropTypes.object,
+    dispatch: PropTypes.func.isRequired,
+    destination: PropTypes.object.isRequired,
+    params: PropTypes.object.isRequired
+};
+
+export default connect(mapStateToProps)(Destination);
