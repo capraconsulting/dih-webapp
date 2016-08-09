faf61a795f6a66ff1d962cc2a5ae61014a01a3d3
Morten Noddeland <morten@noddeland.no>
2016-08-09 09:09:42 +0200

    Add a 'back to frontpage' bar in the 'PublicLayout'

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -3,11 +3,19 @@ import PublicHeader from '../commons/PublicHeader';
 
 function SignUpLayout(props) {
     return (
-        <div className="layout-public">
-            <PublicHeader />
-            <div className="ui card fluid">
-                <div className="content">
-                    {props.children}
+        <div>
+            <div className="to-frontpage">
+                <a href="http://drapenihavet.no">
+                    <i className="icon angle left"></i>
+                    Back to frontpage
+                </a>
+            </div>
+            <div className="layout-public">
+                <PublicHeader />
+                <div className="ui card fluid">
+                    <div className="content">
+                        {props.children}
+                    </div>
                 </div>
             </div>
         </div>
